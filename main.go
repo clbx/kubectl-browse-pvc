ac690d4c51a5bfc535dc748f7bfe64fd7d4128ee
Clay Buxton <clay@clbx.io>
2023-09-17 02:47:48 -0400

    move to func

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/remotecommand"
 	"k8s.io/client-go/util/homedir"
@@ -64,7 +65,7 @@ func main() {
 
 			action := cCtx.Args().Get(0)
 
-			if action != "mount" && action != "unmount" && action != "edit" && action != "archive" {
+			if action != "mount" && action != "unmount" && action != "get" && action != "archive" {
 				fmt.Printf("%s is not a valid action\n", action)
 				os.Exit(1)
 			}
@@ -83,150 +84,130 @@ func main() {
 				os.Exit(1)
 			}
 
-			// Check pods to see if PVC is attached
-
 			nsPods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
 
 			attachedPod := findPodByPVC(*nsPods, *targetPvc)
 
-			// check different setups (atttached, accessmodes, etc.)
 			if attachedPod == nil {
 				fmt.Printf("Not attached to pod\n")
 			} else {
 				fmt.Printf("Attached to %s exiting.\n", attachedPod.Name)
 			}
 
-			//pvcbPod := buildPvcbPod(namespace, *targetPvc)
-
-			// pvcbPod, err = clientset.CoreV1().Pods(namespace).Create(context.TODO(), pvcbPod, metav1.CreateOptions{})
-			// if err != nil {
-			// 	panic(err.Error())
-			// }
-
-			pvcbJob := buildPvcbJob(namespace, *targetPvc)
-
-			pvcbJob, err = clientset.BatchV1().Jobs(namespace).Create(context.TODO(), pvcbJob, metav1.CreateOptions{})
-
-			//.Get(context.TODO(), pvcbJob.GetObjectMeta().GetName(), metav1.GetOptions{})
-
-			if err != nil {
-				panic(err)
+			if action == "get" {
+				get(clientset, config, namespace, *targetPvc)
 			}
-
-			fmt.Printf("Job %s scheduled.\n", pvcbJob.ObjectMeta.Name)
-
-			timeout := 30
-
-			for {
-				pvcbJob, err = clientset.BatchV1().Jobs(namespace).Get(context.TODO(), pvcbJob.GetObjectMeta().GetName(), metav1.GetOptions{})
-
-				if err != nil {
-					panic(err.Error())
-				}
-
-				if pvcbJob.Status.Active > 0 {
-					fmt.Printf("Job is running \n")
-					break
-				}
-
-				fmt.Printf("Not started yet")
-				time.Sleep(time.Second)
-
-			}
-
-			if timeout == 0 {
-				fmt.Printf("Pod failed to start in \n")
-				os.Exit(1)
-			}
-
-			fmt.Printf("Pod started up ")
-
-			//Find created pod
-			podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-				LabelSelector: "job-name=" + pvcbJob.Name,
-			})
-
-			if err != nil {
-				panic(err.Error())
-			}
-
-			if len(podList.Items) != 1 {
-				fmt.Printf("%d\n", len(podList.Items))
-				panic("Didn't find one pod, handle this later")
-			}
-
-			pod := &podList.Items[0]
-
-			// Wait for pod to be ready
-
-			for pod.Status.Phase != corev1.PodRunning && timeout > 0 {
-				fmt.Printf("Waiting for pod. Status: %s\n", pod.Status.Phase)
-
-				pod, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
-				if err != nil {
-					panic(err.Error())
-				}
-
-				time.Sleep(time.Second)
-				timeout--
-			}
-
-			if timeout == 0 {
-				fmt.Printf("Pod failed to start in \n")
-				os.Exit(1)
-			}
-
-			//cmd := []string{"/bin/sh"}
-
-			req := clientset.CoreV1().RESTClient().
-				Post().
-				Resource("pods").
-				Name(pod.Name).
-				Namespace(namespace).
-				SubResource("exec").
-				Param("stdin", "true").
-				Param("stdout", "true").
-				Param("stderr", "true").
-				Param("tty", "true").
-				Param("command", "/bin/bash")
-
-			exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
-			if err != nil {
-				panic(err.Error())
-			}
-
-			oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
-			if err != nil {
-				panic(err)
-			}
-
-			defer terminal.Restore(int(os.Stdin.Fd()), oldState)
-
-			err = exec.Stream(remotecommand.StreamOptions{
-				Stdin:  os.Stdin,
-				Stdout: os.Stdout,
-				Stderr: os.Stderr,
-				Tty:    true,
-			})
-
-			if err != nil {
-				panic(err.Error())
-			}
-
-			// deletePolicy := metav1.DeletePropagationForeground
-			// err = clientset.CoreV1().Pods(namespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{
-			// 	PropagationPolicy: &deletePolicy,
-			// })
-			// if err != nil {
-			// 	panic(err.Error())
-			// }
 
 			return nil
+
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func get(clientset *kubernetes.Clientset, config *rest.Config, namespace string, targetPvc corev1.PersistentVolumeClaim) error {
+
+	// Build the Job
+	pvcbGetJob := buildPvcbGetJob(namespace, targetPvc)
+	// Create Job
+	pvcbGetJob, err := clientset.BatchV1().Jobs(namespace).Create(context.TODO(), pvcbGetJob, metav1.CreateOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	timeout := 30
+
+	// Wait 30 seconds for the Job to start.
+	for timeout > 0 {
+		pvcbGetJob, err = clientset.BatchV1().Jobs(namespace).Get(context.TODO(), pvcbGetJob.GetObjectMeta().GetName(), metav1.GetOptions{})
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if pvcbGetJob.Status.Active > 0 {
+			fmt.Printf("Job is running \n")
+			break
+		}
+
+		fmt.Printf("Not started yet")
+		time.Sleep(time.Second)
+
+		timeout--
+	}
+
+	// Find the created pod.
+	podList, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: "job-name=" + pvcbGetJob.Name,
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if len(podList.Items) != 1 {
+		fmt.Printf("%d\n", len(podList.Items))
+		panic("Found more or less than one pod")
+	}
+
+	pod := &podList.Items[0]
+
+	for pod.Status.Phase != corev1.PodRunning && timeout > 0 {
+		fmt.Printf("Waiting for pod. Status: %s\n", pod.Status.Phase)
+
+		pod, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		time.Sleep(time.Second)
+		timeout--
+	}
+
+	if timeout == 0 {
+		panic("Pod failed to start")
+	}
+
+	req := clientset.CoreV1().RESTClient().
+		Post().
+		Resource("pods").
+		Name(pod.Name).
+		Namespace(namespace).
+		SubResource("exec").
+		Param("stdin", "true").
+		Param("stdout", "true").
+		Param("stderr", "true").
+		Param("tty", "true").
+		Param("command", "/bin/bash")
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
+	defer terminal.Restore(int(os.Stdin.Fd()), oldState)
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+		Tty:    true,
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return nil
 
 }
--- util.go
+++ util.go
@@ -6,6 +6,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// Finds if a pod that attached to a PVC
 func findPodByPVC(podList corev1.PodList, pvc corev1.PersistentVolumeClaim) *corev1.Pod {
 	for _, pod := range podList.Items {
 		for _, volume := range pod.Spec.Volumes {
@@ -17,7 +18,8 @@ func findPodByPVC(podList corev1.PodList, pvc corev1.PersistentVolumeClaim) *cor
 	return nil
 }
 
-func buildPvcbJob(namespace string, pvc corev1.PersistentVolumeClaim) *batchv1.Job {
+// Returns a job for the get command.
+func buildPvcbGetJob(namespace string, pvc corev1.PersistentVolumeClaim) *batchv1.Job {
 
 	TTLSecondsAfterFinished := new(int32)
 	*TTLSecondsAfterFinished = 10
@@ -63,44 +65,5 @@ func buildPvcbJob(namespace string, pvc corev1.PersistentVolumeClaim) *batchv1.J
 			},
 		},
 	}
-
 	return job
-}
-
-func buildPvcbPod(namespace string, pvc corev1.PersistentVolumeClaim) *corev1.Pod {
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pvcb-edit",
-			Namespace: namespace,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "pvcb-edit",
-					Image:   "ghcr.io/clbx/pvcb-edit",
-					Command: []string{"bin/bash", "-c", "--"},
-					Args:    []string{"while true; do sleep 30; done;"},
-					VolumeMounts: []corev1.VolumeMount{
-						{
-							Name:      "target-pvc",
-							MountPath: "/mnt",
-						},
-					},
-				},
-			},
-			Volumes: []corev1.Volume{
-				{
-					Name: "target-pvc",
-					VolumeSource: corev1.VolumeSource{
-						PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-							ClaimName: pvc.Name,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	return pod
-
 }
